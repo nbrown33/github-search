b2d33f219342a806e7fff66a31ed95041d741ade
Nick Brown <nickb1046@gmail.com>
2019-01-20 10:11:18 -0600

    finished

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 "use strict";
+
 const token = "e1fdeca6f0781bceee27d24799ca638b80e4242a"
 
 $("#js-form").submit(event => {
     event.preventDefault();
     let user = $("input").val();
     console.log(user);
-    const options = {
-        headers: new Headers({
-          "Authorization": token})
-      };
-    fetch(`https://api.github.com/users/:${user}/repos`, options)
-    .then(response => response.json())
+    fetch(`https://api.github.com/users/:${user}/repos?access_token=${token}`)
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
     .then(responseJson => console.log(responseJson));
+    .catch(err => {
+        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+    // .then(responseJson => displayResults(responseJson))
 })
+
+function displayResults(responseJson) {
+    $("#results").empty();
+    for (let i=0; i < responseJson.message.length; i++) {
+        $("#results-list").append(`<li></li>`)
+    }
+    $("#results").removeClass("hidden")
+}
