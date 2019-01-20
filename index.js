36ccbe53946395e97dc518545e7cfb3212d058eb
Nick Brown <nickb1046@gmail.com>
2019-01-20 10:11:48 -0600

    ok really finished now

--- index.js
+++ index.js
@@ -4,26 +4,34 @@ const token = "e1fdeca6f0781bceee27d24799ca638b80e4242a"
 
 $("#js-form").submit(event => {
     event.preventDefault();
+    $("#results-list").empty()
+    $("#js-error-message").empty()
     let user = $("input").val();
-    console.log(user);
-    fetch(`https://api.github.com/users/:${user}/repos?access_token=${token}`)
+    fetch(`https://api.github.com/users/${user}/repos`, {
+      headers:{
+        'Authorization': `token ${token}`
+      }
+    })
     .then(response => {
         if (response.ok) {
             return response.json();
         }
         throw new Error(response.statusText);
     })
-    .then(responseJson => console.log(responseJson));
+    .then(responseJson => { 
+      console.log(responseJson[0].name)
+      displayResults(responseJson)
+    })
     .catch(err => {
+        console.log(err)
         $('#js-error-message').text(`Something went wrong: ${err.message}`);
     });
-    // .then(responseJson => displayResults(responseJson))
 })
 
 function displayResults(responseJson) {
-    $("#results").empty();
-    for (let i=0; i < responseJson.message.length; i++) {
-        $("#results-list").append(`<li></li>`)
+    let results = responseJson;
+    for (let i=0; i < results.length; i++) {
+        $("#results-list").append(`<li>${results[i].name}<br/><a href="${results[i].html_url}">${results[i].html_url}</li><br/>`)
     }
-    $("#results").removeClass("hidden")
+    $("#results").removeClass("hidden");
 }
